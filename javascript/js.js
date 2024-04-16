c90b39668ee67388fe643df62b2405fbbcd3f8e4
vadrian1 <123913872+vadrian1@users.noreply.github.com>
2024-04-16 00:46:06 -0400

    prsesentaiton

--- javascript/js.js
+++ javascript/js.js
@@ -11,16 +11,21 @@ showSlide(1);
 
 function plusSlides(index){
     showSlide(currentSlide+=index);
+    console.log("interval");
 }
 function showSlide(index){
     if(index > slides.length){
         currentSlide = 1
     }
     if(index < 1){
-        currentSlide = slide.length;
+        currentSlide = slides.length;
     }
     for(let i = 0; i < slides.length; ++i){
         slides[i].style.display = 'none';
     }
     slides[currentSlide - 1].style.display = 'block';
 }
+
+const interval = setInterval(function(){
+    plusSlides(1)
+}, 5000)
